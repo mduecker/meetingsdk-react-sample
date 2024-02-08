1c1d4d3ddf0101e7a673f3600bae98946f440446
Markus Dücker <markus@joinhandshake.com>
2024-02-08 13:44:02 +0100

    Adding /auth endpoint as Vercel function

---
+++ api/auth.js
@@ -0,0 +1,31 @@
+const KJUR = require("jsrsasign");
+
+export default function handler(req, res) {
+  const iat = Math.round(new Date().getTime() / 1000) - 30;
+  const exp = iat + 60 * 60 * 2;
+
+  const oHeader = { alg: "HS256", typ: "JWT" };
+
+  const oPayload = {
+    sdkKey: process.env.ZOOM_MEETING_SDK_KEY,
+    mn: req.body.meetingNumber,
+    role: req.body.role,
+    iat: iat,
+    exp: exp,
+    appKey: process.env.ZOOM_MEETING_SDK_KEY,
+    tokenExp: iat + 60 * 60 * 2,
+  };
+
+  const sHeader = JSON.stringify(oHeader);
+  const sPayload = JSON.stringify(oPayload);
+  const signature = KJUR.jws.JWS.sign(
+    "HS256",
+    sHeader,
+    sPayload,
+    process.env.ZOOM_MEETING_SDK_SECRET
+  );
+
+  res.json({
+    signature: signature,
+  });
+}
